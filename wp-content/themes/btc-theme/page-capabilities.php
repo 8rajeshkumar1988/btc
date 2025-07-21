cf20b044452d4a3b7e0606ebe8a0f2633d6615c7
Arjun1Alpha <arjun.thakur@thealphagency.com>
2025-07-21 16:43:04 +0530

    add

--- wp-content/themes/btc-theme/page-capabilities.php
+++ wp-content/themes/btc-theme/page-capabilities.php
@@ -3,7 +3,294 @@
 get_header();
 
 ?>
-Capabilities
+<section class="heroBanner">
+      <!-- <video
+        playsinline
+        autoplay
+        muted
+        loop
+        src="../assets/Sustainbility.mp4"></video> -->
+      <img src="<?php echo get_template_directory_uri() . '/assets/images/verticle/Sustainbility.jpg'; ?>" alt="" />
+      <div class="content">
+            <p class="breadcrub"><a href="<?php echo site_url('/') ?>">Home</a>  / <?php the_title() ?></p>
+
+        <div class="heading">
+          <p>OUR CAPABILITIES</p>
+          <h1>
+            Crafted for Quality <br />
+            and Sustainability
+          </h1>
+        </div>
+        <div class="layer"></div>
+      </div>
+    </section>
+
+    <section id="vertical_integration">
+      <img src="<?php echo get_template_directory_uri() . '/assets/images/BTC_pattern.png'; ?>" alt="" />
+      <div class="heading">
+        <h2>Vertical Integration</h2>
+        <p>
+          BTC combines state-of-the-art technology with sustainable practices,
+          ensuring every product is crafted with care, precision and
+          environmental responsibility.
+        </p>
+      </div>
+      <div class="cards_wrapper">
+        <div class="card">
+          <img src="<?php echo get_template_directory_uri() . '/assets/images/verticle/sec.jpg'; ?>" alt="" />
+          <div class="card_info">
+            <div class="card_head">
+              <div class="icon">
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/verticle/first_icon.png'; ?>" alt="" />
+              </div>
+              <h3 class="title">Spinning</h3>
+            </div>
+            <div class="card_body">
+              <h3>
+                <span>32,832</span> <br />
+                High-Performance <br> Spindles
+              </h3>
+              <div class="card_desc">
+                <p>Machinery Partner</p>
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/verticle/reter.png'; ?>" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <img src="<?php echo get_template_directory_uri() . '/assets/images/verticle/first.jpg'; ?>" alt="" />
+          <div class="card_info">
+            <div class="card_head">
+              <div class="icon">
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/verticle/knitting.png'; ?>" alt="" />
+              </div>
+              <h3 class="title">Knitting</h3>
+            </div>
+            <div class="card_body">
+              <h3>
+                <span>83</span> <br />
+                Knitting & Collar <br> Machines
+              </h3>
+              <div class="card_desc">
+                <p>Machinery Partner</p>
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/verticle/terrot.png'; ?>" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <img src="<?php echo get_template_directory_uri() . '/assets/images/verticle/dyeing_machines.jpg'; ?>" alt="" />
+          <div class="card_info">
+            <div class="card_head">
+              <div class="icon">
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/verticle/dyeing.png'; ?>" alt="" />
+              </div>
+              <h3 class="title">Dyeing</h3>
+            </div>
+            <div class="card_body">
+              <h3>
+                <span>14</span> <br />
+                Dyeing <br> Machines
+              </h3>
+              <div class="card_desc">
+                <p>Machinery Partner</p>
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/verticle/thiess.png'; ?>" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <img src="<?php echo get_template_directory_uri() . '/assets/images/verticle/printing.jpg'; ?>" alt="" />
+          <div class="card_info">
+            <div class="card_head">
+              <div class="icon">
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/verticle/gear.png'; ?>" alt="" />
+              </div>
+              <h3 class="title">Printing</h3>
+            </div>
+            <div class="card_body">
+              <h3>
+                <span>14</span> <br />Printing <br> Machines
+              </h3>
+              <div class="card_desc">
+                <p>Machinery Partner</p>
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/verticle/thiess.png'; ?>" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <img src="<?php echo get_template_directory_uri() . '/assets/images/verticle/embroidery.png'; ?>" alt="" />
+          <div class="card_info">
+            <div class="card_head">
+              <div class="icon">
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/verticle/embroidery_clock.png'; ?>" alt="" />
+              </div>
+              <h3 class="title">Embroidery</h3>
+            </div>
+            <div class="card_body">
+              <h3>
+                <span>9 Needle <br> 18 Head </span> <br />
+                Computerized <br> Machine
+              </h3>
+              <div class="card_desc">
+                <p>Machinery Partner</p>
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/verticle/barudan.png'; ?>" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <img src="<?php echo get_template_directory_uri() . '/assets/images/verticle/stitching.jpg'; ?>" alt="" />
+          <div class="card_info">
+            <div class="card_head">
+              <div class="icon">
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/verticle/stitching_machine.png'; ?>" alt="" />
+              </div>
+              <h3 class="title">Stitching</h3>
+            </div>
+            <div class="card_body">
+              <h3>
+                <span>1391</span> <br />
+                Stitching <br> Machines
+              </h3>
+              <div class="card_desc">
+                <p>Machinery Partner</p>
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/verticle/Juki.png'; ?>" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <img src="<?php echo get_template_directory_uri() . '/assets/images/verticle/laundry.jpg'; ?>" alt="" />
+          <div class="card_info">
+            <div class="card_head">
+              <div class="icon">
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/verticle/laundry_icon.png'; ?>" alt="" />
+              </div>
+              <h3 class="title">Laundry</h3>
+            </div>
+            <div class="card_body">
+              <h3>
+                <span>16</span> <br />
+                Laundry <br> Machines 
+              </h3>
+              <div class="card_desc">
+                <p>Machinery  Partner</p>
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/verticle/ramsons.png'; ?>" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="sustainability_details">
+      <div class="sustainability_info">
+        <div>
+          <div class="heading">
+            <h2>Sustainability</h2>
+          </div>
+          <p>
+            We integrate sustainable practices at every step, reducing our
+            carbon footprint and waste.
+          </p>
+          <ul>
+            <li>CmiA-certified cotton for eco-friendly sourcing.</li>
+            <li>Zero Liquid Discharge (ZLD) technology for water recycling.</li>
+            <li>Clean energy and eco-conscious manufacturing processes.</li>
+          </ul>
+        </div>
+        <div class="image_wrapper">
+          <img src="<?php echo get_template_directory_uri() . '/assets/images/verticle/cotton.jpg'; ?>" alt="" />
+        </div>
+      </div>
+      <div class="technology_and_innovation">
+        <div class="heading">
+          <h2>Technology and Innovation</h2>
+        </div>
+        <div class="tai_container">
+          <div class="static">
+            <div>
+              <h3>2782 m/c</h3>
+              <p>Production with 1391 Stitching machines x 2 shifts</p>
+            </div>
+            <div>
+              <h3>11.4 Ha</h3>
+              <p>Vertically Integrated Knitting unit area</p>
+            </div>
+            <div>
+              <h3>7000 Tons</h3>
+              <p>Knit fabric production annually</p>
+            </div>
+            <div>
+              <h3>24 Mn</h3>
+              <p>Pieces of apparel per annum</p>
+            </div>
+          </div>
+          <div class="dynamic_desc">
+            <p>
+              Using cutting-edge technology, we ensure fast, reliable and
+              eco-friendly production.
+            </p>
+            <ul>
+              <li>Blockchain technology for end-to-end traceability.</li>
+              <li>Advanced machinery for precision and speed.</li>
+              <li>Automated design and sampling (3D capabilities).</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="logistics_shipping_efficiency">
+      <img src="<?php echo get_template_directory_uri() . '/assets/images/BTC_pattern.png'; ?>" alt="" />
+
+      <div class="logistics_shipping_efficiency_head">
+        <div class="heading">
+          <h2>Logistics & Shipping Efficiency</h2>
+          <p style="font-weight: 300; text-transform: none">
+            Strategically located in West Africa, our proximity to <br />
+            major markets ensures timely delivery.
+          </p>
+        </div>
+      </div>
+      <div class="logistics_shipping_efficiency_container">
+        <div class="category_item">
+          <h2>Airport</h2>
+          <div class="category_image">
+            <img src="<?php echo get_template_directory_uri() . '/assets/images/verticle/airport.jpg'; ?>" alt="" />
+          </div>
+          <p class="category_description">
+            We offer direct air connectivity to key destinations across
+            <span>the USA and the European Union,</span> ensuring seamless
+            logistics and reduced transit times.
+          </p>
+        </div>
+        <div class="category_item">
+          <h2>Sea Port</h2>
+          <div class="category_image">
+            <img src="<?php echo get_template_directory_uri() . '/assets/images/verticle/sea_port.jpg'; ?>" alt="" />
+          </div>
+          <p class="category_description">
+            Cotonou Sea Port access within <span>40kms/30kms</span> Goods
+            produced can be loaded and shipped to port in 30mins.
+          </p>
+        </div>
+        <div class="category_item">
+          <h2>Dry Port</h2>
+          <div class="category_image">
+            <img src="<?php echo get_template_directory_uri() . '/assets/images/verticle/dry_port.jpg'; ?>" alt="" />
+          </div>
+          <p class="category_description">
+            Dry Port
+            <span>inside the zone where custom clearance happens,</span> without
+            hassles. In other countries, the custom clearance happens at port.
+          </p>
+        </div>
+        <!-- <div class="swiper-pagination"></div> -->
+      </div>
+    </section>
 
 <?php
 get_footer();
