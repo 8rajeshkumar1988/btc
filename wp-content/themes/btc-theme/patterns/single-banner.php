73a3b8f90fc8b14deecdb7f7a004665c68701c5e
Rajesh Kumar <rajesh.kumar@thealphagency.com>
2025-07-23 13:16:24 +0530

    latets

---
+++ wp-content/themes/btc-theme/patterns/single-banner.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Title: BTC Custom Single Banner
+ * Slug: single-banner
+ * Categories: post
+ */
+?>
+
+<section class="blog_hero">
+    <div>
+        <img src="<?php echo get_template_directory_uri() . '/assets/images/cotton.jpg'; ?>" alt="title" />
+        <div class="content">
+            <div class="heading">
+                <p>Blog type</p>
+                <h1>
+                    Africa's Inner & Active Wear Edge: <br />
+                    A New Investment Frontier
+                </h1>
+            </div>
+        </div>
+    </div>
+
+
+</section>
---
+++ wp-content/themes/btc-theme/patterns/two-paragraph.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Title: BTC Custom Two Paragraph Section
+ * Slug: btc-theme/two-paragraph
+ * Categories: post
+ */
+?>
+<div>
+    <div class="Left_section">
+        Left Section
+    </div>
+    <div class="right_section">
+        Rught Section
+    </div>
+</div>
