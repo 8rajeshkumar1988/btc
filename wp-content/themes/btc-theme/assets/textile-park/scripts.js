873d4db9063f546b6cad0e3cbbbf642d60679eb7
Yashwant Rao <raoyashwant024@gmail.com>
2025-07-31 15:33:32 +0530

    a

---
+++ wp-content/themes/btc-theme/assets/textile-park/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+//    $('#parking .containers').each(function () { 
+//     gsap.to(this, {
+//         transform: ' rotateY(-5deg) rotateX(5deg) scale(0.7, 0.7)',
+//         filter: 'blur(0px) brightness(0.8)',
+//         ease: "none",
+//         scrollTrigger: {
+//             trigger: this,
+//             start: "top -50%", 
+            
+//             scrub: true,
+//         }
+//     })
+//    })
+});
