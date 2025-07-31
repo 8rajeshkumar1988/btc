44ea4c72d542859fa085d6b391c712a8b17ba76f
Arjun1Alpha <arjun.thakur@thealphagency.com>
2025-07-31 19:30:35 +0530

    add

--- wp-content/themes/btc-theme/components/socials.php
+++ wp-content/themes/btc-theme/components/socials.php
@@ -65,6 +65,57 @@
             }
             wp_reset_postdata();
             ?>
+            <?php
+
+            $clients = new WP_Query(array(
+                'posts_per_page' => -1,
+                'post_type' => 'socialmedia',
+                'orderby' => 'title',
+                'post_status'    => 'publish',
+                'meta_key'       => '_sort_order',
+                'orderby'        => 'meta_value_num',
+                'order'          => 'ASC',
+
+            ));
+            if ($clients->have_posts()) {
+                while ($clients->have_posts()) {
+                    $clients->the_post();
+
+                    $social_icon = get_field('social_icon');
+                    $embed_code = get_field('embed_code');
+                    if ($embed_code) {
+                        echo $embed_code;
+                    } else {  ?>
+                        <a target="_blank" href="<?php echo get_field('media_url'); ?>" class="swiper-slide social_media_card">
+                            <?php
+                            $thumbnail_id = get_post_thumbnail_id();
+                            $image_url = wp_get_attachment_url($thumbnail_id);
+                            $alt_text = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
+                            $title_text = get_the_title($thumbnail_id);
+                            if (empty($alt_text)) {
+                                $alt_text = get_the_title();
+                            }
+                            if (empty($title_text)) {
+                                $title_text = get_the_title();
+                            }
+
+                            $image = '<img class="socialmedia__image"  src="' . esc_url($image_url) . '" alt="' . esc_attr($alt_text) . '">';
+                            echo  $image;
+
+                            ?>
+
+                            <?php if ($social_icon) { ?>
+                                <div class="social_icon">
+                                    <?php echo '<img src="' . esc_url($social_icon['url']) . '" alt="' . esc_attr($social_icon['alt']) . '">'; ?>
+                                </div>
+                            <?php } ?>
+                        </a>
+                    <?php } ?>
+            <?php
+                }
+            }
+            wp_reset_postdata();
+            ?>
         </div>
     </div>
 
@@ -83,10 +134,10 @@
         loop: true,
         
          autoplay: {
-            delay: 2500,
+            delay: 2000,
             disableOnInteraction: false,
         },
-        speed: 600,
+        speed: 1000,
         breakpoints: {
             480: {
                 slidesPerView: "auto",
