45a65f8c99eb2c5933f4963ba757d46462eb1374
Rajesh Kumar <rajesh.kumar@thealphagency.com>
2025-08-01 11:03:34 +0530

    ff

--- wp-content/themes/btc-theme/single-category.php
+++ wp-content/themes/btc-theme/single-category.php
@@ -4,28 +4,25 @@ get_header();
 the_post();
 ?>
 <section class="heroBanner">
-<?php
-    $banner_video = get_field('banner_video');
-      $thumbnail_id = get_post_thumbnail_id();
-        $image_url = wp_get_attachment_url($thumbnail_id);
-        $alt_text = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
-        $title_text = get_the_title($thumbnail_id);
-        if (empty($alt_text)) {
-            $alt_text = get_the_title();
-        }
-        if (empty($title_text)) {
-            $title_text = get_the_title();
-        }
+  <?php
+  $banner_video = get_field('banner_video');
+  $banner_image = get_field('banner_image');
+  if ($banner_image) {
+    $image_url = isset($banner_image['sizes']['full']) ? $banner_image['sizes']['full'] : $banner_image['url'];
+    $alt_text = isset($banner_image['alt']) ? $banner_image['alt'] : '';
+  } else {
+    $image_url = "";
+    $alt_text = the_title();
+  }
 
+  if ($banner_video) {
+    echo '<video playsinline autoplay muted loop poster="' . $image_url . '" src="' . esc_url($banner_video['url']) . '"></video>';
+  } else  if (has_post_thumbnail()) {
+    $image = '<img  src="' . esc_url($image_url) . '" alt="' . esc_attr($alt_text) . '">';
+    echo $image;
+  }
 
-    if ($banner_video) {
-        echo '<video playsinline autoplay muted loop poster="'.$image_url.'" src="' . esc_url($banner_video['url']) . '"></video>';
-    } else  if (has_post_thumbnail()) {
-        $image = '<img  src="' . esc_url($image_url) . '" alt="' . esc_attr($alt_text) . '">';
-        echo $image;
-    }
-
-    ?>
+  ?>
 
 
   <div class="content">
