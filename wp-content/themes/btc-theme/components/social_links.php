b260b14ae03d8cfe4292887a2f083420abb90231
Arjun1Alpha <arjun.thakur@thealphagency.com>
2025-08-23 17:42:24 +0530

    add

--- wp-content/themes/btc-theme/components/social_links.php
+++ wp-content/themes/btc-theme/components/social_links.php
@@ -2,6 +2,6 @@
      <a href=""><img src="<?php echo get_template_directory_uri() . '/assets/images/insta.png'; ?>" alt="instagram"></a>
      <a href=""><img src="<?php echo get_template_directory_uri() . '/assets/images/facebook.png'; ?>" alt="facebook"></a>
      <a href=""><img src="<?php echo get_template_directory_uri() . '/assets/images/x.png'; ?>" alt="twitter"></a>
-     <a href=""><img src="<?php echo get_template_directory_uri() . '/assets/images/linkedin.png'; ?>" alt="linkedin"></a>
+     <a href="https://www.linkedin.com/company/benin-textile-corporation/" target="_blank"><img src="<?php echo get_template_directory_uri() . '/assets/images/linkedin.png'; ?>" alt="linkedin"></a>
      <a href=""><img src="<?php echo get_template_directory_uri() . '/assets/images/youtube.png'; ?>" alt="youtube"></a>
  </div>
