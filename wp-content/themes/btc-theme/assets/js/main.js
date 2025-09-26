15a901feab363e8131b39ce06e7fbf1676611239
Yashwant Rao <raoyashwant024@gmail.com>
2025-09-26 15:33:34 +0530

    a

--- wp-content/themes/btc-theme/assets/js/main.js
+++ wp-content/themes/btc-theme/assets/js/main.js
@@ -501,36 +501,44 @@ $(document).on("scroll", function () {
 
 
 $(document).ready(function () {
-   function isOverlapping($div1, $div2) {
-    const d1 = $div1[0].getBoundingClientRect();
-    const d2 = $div2[0].getBoundingClientRect();
+  function isOverlapping(div1, div2) {
+  const d1 = div1.getBoundingClientRect();
+  const d2 = div2.getBoundingClientRect();
 
-    return !(
-      d1.right < d2.left ||
-      d1.left > d2.right ||
-      d1.bottom < d2.top ||
-      d1.top > d2.bottom
-    );
-  }
+  return !(
+    d1.right < d2.left ||
+    d1.left > d2.right ||
+    d1.bottom < d2.top ||
+    d1.top > d2.bottom
+  );
+}
 
-  function checkOverlap() {
-    const $fixedBtn = $('.backtoTop');
-    let overlap = false;
+function checkOverlap() {
+  const fixedBtn = document.querySelector('.backtoTop');
+  const targets = document.querySelectorAll('.globalNavigation, .cta');
+  let overlap = false;
 
-    $('.globalNavigation, .cta').each(function () {
-      if (isOverlapping($fixedBtn, $(this))) {
-        overlap = true;
-      }
-    });
-
-    if (overlap) {
-      $fixedBtn.css({ opacity: 0, visibility: "hidden", pointerEvents: "none" });
-    } else {
-      $fixedBtn.css({ opacity: 1, visibility: "visible", pointerEvents: "auto" });
+  targets.forEach(el => {
+    if (isOverlapping(fixedBtn, el)) {
+      overlap = true;
     }
-  }
+  });
 
-  $(window).on('scroll resize', checkOverlap);
-  checkOverlap();
+  if (overlap) {
+    fixedBtn.style.opacity = "0";
+    fixedBtn.style.visibility = "hidden";
+    fixedBtn.style.pointerEvents = "none";
+  } else {
+    fixedBtn.style.opacity = "1";
+    fixedBtn.style.visibility = "visible";
+    fixedBtn.style.pointerEvents = "auto";
+  }
+}
+
+window.addEventListener('scroll', checkOverlap);
+window.addEventListener('resize', checkOverlap);
+
+// Run on load
+checkOverlap();
 
 })
