de194c7bea62143f4ec8b7a2f5da7d4ce20242dd
Yashwant Rao <raoyashwant024@gmail.com>
2026-08-13 11:25:42 +0530

    a

--- wp-content/themes/btc-theme/components/social_links.php
+++ wp-content/themes/btc-theme/components/social_links.php
@@ -1,7 +1,8 @@
  <div>
-     <!-- <a href=""><img src="<?php echo get_template_directory_uri() . '/assets/images/insta.png'; ?>" alt="instagram"></a>
-     <a href=""><img src="<?php echo get_template_directory_uri() . '/assets/images/facebook.png'; ?>" alt="facebook"></a>
-     <a href=""><img src="<?php echo get_template_directory_uri() . '/assets/images/x.png'; ?>" alt="twitter"></a> -->
+     <a href="https://www.instagram.com/benintextilecorporation_btc?utm_source=qr&igsh=d2w5ZDI0c2hhYWQ5 "><img src="<?php echo get_template_directory_uri() . '/assets/images/insta.png'; ?>" alt="instagram"></a>
+     <a href="https://www.facebook.com/share/1FE9cQB9Dj/"><img src="<?php echo get_template_directory_uri() . '/assets/images/facebook.png'; ?>" alt="facebook"></a>
+     <!-- <a href=""><img src="<?php echo get_template_directory_uri() . '/assets/images/x.png'; ?>" alt="twitter"></a> -->
      <a href="https://www.linkedin.com/company/benin-textile-corporation/" target="_blank"><img src="<?php echo get_template_directory_uri() . '/assets/images/linkedin.png'; ?>" alt="linkedin"></a>
      <!-- <a href=""><img src="<?php echo get_template_directory_uri() . '/assets/images/youtube.png'; ?>" alt="youtube"></a> -->
+     <a href="https://www.tiktok.com/@benin.textile.corp?_r=1&_t=ZS-98h0MElIpTZ"><img src="<?php echo get_template_directory_uri() . '/assets/images/tiktok.png'; ?>" alt="youtube"></a>
  </div>
