e651eb1f689abe0d318bba92bf24d96270aae91c
Yashwant Rao <raoyashwant024@gmail.com>
2026-08-13 13:25:16 +0530

    a

--- wp-content/themes/btc-theme/components/headerMenu.php
+++ wp-content/themes/btc-theme/components/headerMenu.php
@@ -324,12 +324,12 @@
 
         <div class="mediaArea">
             <div>
-                <a href="https://www.instagram.com/benintextilecorporation_btc?utm_source=qr&igsh=d2w5ZDI0c2hhYWQ5 "><img src="<?php echo get_template_directory_uri() . '/assets/images/insta.png'; ?>" alt="instagram"></a>
-                <a href="https://www.facebook.com/share/1FE9cQB9Dj/"><img src="<?php echo get_template_directory_uri() . '/assets/images/facebook.png'; ?>" alt="facebook"></a>
+                <a target="_blank" href="https://www.instagram.com/benintextilecorporation_btc?utm_source=qr&igsh=d2w5ZDI0c2hhYWQ5 "><img src="<?php echo get_template_directory_uri() . '/assets/images/insta.png'; ?>" alt="instagram"></a>
+                <a target="_blank" href="https://www.facebook.com/share/1FE9cQB9Dj/"><img src="<?php echo get_template_directory_uri() . '/assets/images/facebook.png'; ?>" alt="facebook"></a>
                     
                 <a href="https://www.linkedin.com/company/benin-textile-corporation/" target="_blank"><img src="<?php echo get_template_directory_uri() . '/assets/images/linkedin.png'; ?>"
                         alt="linkedin"></a>
-                <a href="https://www.tiktok.com/@benin.textile.corp?_r=1&_t=ZS-98h0MElIpTZ"><img src="<?php echo get_template_directory_uri() . '/assets/images/tiktok.png'; ?>" alt="youtube"></a>
+                <a target="_blank" href="https://www.tiktok.com/@benin.textile.corp?_r=1&_t=ZS-98h0MElIpTZ"><img src="<?php echo get_template_directory_uri() . '/assets/images/tiktok.png'; ?>" alt="youtube"></a>
             </div>
             <p>Copyright© 2026, Benin Textile Corporation</p>
         </div>
